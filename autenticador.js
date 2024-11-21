7cbbb7deb1b609b9c7a7def93b2ac2e7b20f404c
Ariel65536 <60203045+Ariel65536@users.noreply.github.com>
2024-11-21 16:57:10 -0300

    Update autenticador.js

--- autenticador.js
+++ autenticador.js
@@ -1,26 +1,31 @@
 const { Client } = require('pg');
+const util = require("./util/util");
+const chalk = require('chalk');
 
 class autenticador {
-    constructor() {
-        console.log("Autenticador!");
-        const connectionData = {
-            user: process.env.USUARIO_POSTGRES,
-            host: 'localhost',
-            database: process.env.BASEDATOS_POSTGRES,
-            password: process.env.CONTRA_POSTGRES,
-            port: 5432,
-        }
-        const client = new Client(connectionData)
+    BaseDatos;
+    constructor(ServidorHttps, ServidorHttp, BaseDatos) {
+        this.BaseDatos = BaseDatos;
+    }
 
-        client.connect()
-        client.query('SELECT * FROM Usuarios')
-            .then(response => {
-                console.log(response.rows)
-                client.end()
-            })
-            .catch(err => {
-                client.end()
-            })
+    async configurar() {
+        try {
+            if (await this.BaseDatos.existeTabla("usuarios")) {
+                console.log("Base de datos Usuarios ya existe");
+            } else {
+                console.log("Base de datos Usuarios no existe, creando...");
+                await this.BaseDatos.crearTabla("usuarios");
+            }
+            util.consola(chalk.green, "Autenticador", "Inicio", "Tabla de usuarios verificada");
+
+        } catch (error) {
+            util.consola(chalk.red, "Autenticador", "Error", "Algo paso al crear tabla usuarios");
+            console.log(error);
+        }
+    }
+
+    agregarUsuario(Nombre, Contrasena, Permisos){
+       
     }
 }
 
